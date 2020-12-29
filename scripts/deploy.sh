470dfa26c0f170672598311baa45a65227012d90
augustin <doat.augustin@gmail.com>
2020-12-29 18:36:53 +0100

    add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ng build --prod
+cd dist/optometrie-angular
+zip grigori-optometrie.zip * -r 
+scp -P 18 grigori-optometrie.zip augustin@92.222.70.14:/var/www/grigori-optometrie
+ssh -p 18 augustin@92.222.70.14 unzip -o /var/www/grigori-optometrie/grigori-optometrie.zip -d /var/www/grigori-optometrie/
